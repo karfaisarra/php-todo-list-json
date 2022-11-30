5e3470b5fc4632f6c6ac61093b58a1faaee69d55
karfaisarra <karfaisarra@gmail.com>
2022-11-30 17:09:47 +0100

    add changes

--- server.php
+++ server.php
@@ -7,6 +7,8 @@ $tasks = json_decode($tasks_json);
 if (isset($_POST['addTask'])) {
     $task = $_POST['addTask'];
     array_push($tasks, $task);
+    $newJsonTasks = json_encode($tasks);
+    file_put_contents('tasks.json', $newJsonTasks);
 }
 header('Content-Type: application/json');
 echo json_encode($tasks);
